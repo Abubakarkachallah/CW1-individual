cc2c2d35703d95cc1da3c6283719644879eb5761
Farouk <kachallah27@yahoo.com>
2020-11-17 18:42:42 +0400

    Added products to lessons.js file

---
+++ lessons.js
@@ -0,0 +1,82 @@
+let products = [
+    {
+        "id": 1001,
+        "title": "ADVANCED CHEMISTRY",
+        "price": 30.00,
+        "image": "#",
+        "availableInventory": 20,
+        "rating": 5
+    },
+    {
+        "id": 1002,
+        "title": "MATHEMATICS",
+        "price": 10.00,
+        "image": "#",
+        "availableInventory": 10,
+        "rating": 2
+    },
+    {
+        "id": 1003,
+        "title": "SOFTWARE ENGINEERING PRINCIPLES",
+        "price": 50.00,
+        "image": "#",
+        "availableInventory": 35,
+        "rating": 5
+    },
+    {
+        "id": 1004,
+        "title": "ENGLISH STUDIES",
+        "price": 10.00,
+        "image": "#",
+        "availableInventory": 40,
+        "rating": 3
+    },
+    {
+        "id": 1005,
+        "title": "HOW TO BE A FOOTBALLER",
+        "price": 30.00,
+        "image": "#",
+        "availableInventory": 15,
+        "rating": 4
+    },
+    {
+        "id": 1006,
+        "title": "EDUCATED",
+        "price": 15.00,
+        "image": "#",
+        "availableInventory": 20,
+        "rating": 3
+    },
+    {
+        "id": 1007,
+        "title": "BUSINESS MANAGEMENT",
+        "price": 20.00,
+        "image": "#",
+        "availableInventory": 20,
+        "rating": 4
+    },
+    {
+        "id": 1008,
+        "title": "INFORMATION TECHNOLOGY",
+        "price": 35.00,
+        "image": "#",
+        "availableInventory": 20,
+        "rating": 5
+    },
+    {
+        "id": 1009,
+        "title": "PINGFONG BABY(KIDS)",
+        "price": 5.00,
+        "image": "#",
+        "availableInventory": 5,
+        "rating": 1
+    },
+    {
+        "id": 1010,
+        "title": "THOR",
+        "price": 50.00,
+        "image": "#",
+        "availableInventory": 20,
+        "rating": 5
+    }
+]
