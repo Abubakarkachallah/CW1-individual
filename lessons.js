05f532167216befb8bc8456094aa50e2d75ac0f2
Farouk <kachallah27@yahoo.com>
2020-11-17 23:45:58 +0400

    Displayed products

--- lessons.js
+++ lessons.js
@@ -2,80 +2,90 @@ let products = [
     {
         "id": 1001,
         "title": "ADVANCED CHEMISTRY",
+        "location": "LONDON",
         "price": 30.00,
-        "image": "#",
+        "image": "images/AC.jpg",
         "availableInventory": 20,
         "rating": 5
     },
     {
         "id": 1002,
         "title": "MATHEMATICS",
+        "location": "MAURITIUS",
         "price": 10.00,
-        "image": "#",
+        "image": "images/Math.jpg",
         "availableInventory": 10,
         "rating": 2
     },
     {
         "id": 1003,
         "title": "SOFTWARE ENGINEERING PRINCIPLES",
+        "location": "MALTA",
         "price": 50.00,
-        "image": "#",
+        "image": "images/SE.jpg",
         "availableInventory": 35,
         "rating": 5
     },
     {
         "id": 1004,
         "title": "ENGLISH STUDIES",
+        "location": "MALI",
         "price": 10.00,
-        "image": "#",
+        "image": "images/English.jpeg",
         "availableInventory": 40,
         "rating": 3
     },
     {
         "id": 1005,
         "title": "HOW TO BE A FOOTBALLER",
+        "location": "SPAIN",
         "price": 30.00,
-        "image": "#",
+        "image": "images/Football.jpeg",
         "availableInventory": 15,
         "rating": 4
     },
     {
         "id": 1006,
         "title": "EDUCATED",
+        "location": "BRAZIL",
         "price": 15.00,
-        "image": "#",
+        "image": "images/ED.jpeg",
         "availableInventory": 20,
         "rating": 3
     },
     {
         "id": 1007,
         "title": "BUSINESS MANAGEMENT",
+        "location": "PORTUGAL",
         "price": 20.00,
-        "image": "#",
+        "image": "images/BM.jpeg",
         "availableInventory": 20,
         "rating": 4
     },
     {
         "id": 1008,
         "title": "INFORMATION TECHNOLOGY",
+        "location": "FRANCE",
         "price": 35.00,
-        "image": "#",
+        "image": "images/IT.jpeg",
         "availableInventory": 20,
         "rating": 5
     },
     {
         "id": 1009,
         "title": "PINGFONG BABY(KIDS)",
+        "location": "SWITZERLAND",
         "price": 5.00,
-        "image": "#",
+        "image": "images/PF.png",
         "availableInventory": 5,
         "rating": 1
     },
     {
         "id": 1010,
         "title": "THOR",
+        "location": "AMERICA",
         "price": 50.00,
-        "image": "#",
+        "image": "images/Thor.jpeg",
         "availableInventory": 20,
         "rating": 5
     }
